e8d53e3d8e0bc7fc486659d9ee72857eedbc5372
adapole <eyassubn@gmail.com>
2023-03-25 01:13:47 +0300

    Swagger added for backend API docs

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,12 @@
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     description: Returns the hello world
+ *     responses:
+ *       200:
+ *         description: hello world
+ */
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+	return new Response('Hello, Next.js!');
 }
---
+++ app/swagger/page.tsx
@@ -0,0 +1,21 @@
+import { createSwaggerSpec } from 'next-swagger-doc';
+import SwaggerClient from './SwaggerClient';
+
+const getApiDocs = async () => {
+	const spec: Record<string, any> = createSwaggerSpec({
+		apiFolder: '/app/api',
+		definition: {
+			openapi: '3.0.0',
+			info: {
+				title: 'Next Swagger API Example',
+				version: '1.0',
+			},
+		},
+	});
+	return spec;
+};
+
+export default async function Home() {
+	const spec = await getApiDocs();
+	return <SwaggerClient spec={spec} />;
+}
