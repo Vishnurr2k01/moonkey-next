51142f7cb1a8289612019baefb6ae3e97ebf0b59
Muu79 <muu794@gmail.com>
2023-04-03 20:28:45 +0200

    fixed import path

--- app/(main)/moons/assets/page.tsx
+++ app/(main)/moons/assets/page.tsx
@@ -1,5 +1,8 @@
+import Uniswap from '../../../../components/UniSwap'
 function page() {
-	return <div className='col-span-6 bg-[#F7F7F7] h-full'>Assets</div>;
+	return <div className='col-span-6 bg-[#F7F7F7] h-full'>
+		<Uniswap />
+	</div>;
 }
 
 export default page;
