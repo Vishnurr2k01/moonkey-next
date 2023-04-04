ff9f9f2b1af906fb2d45373cfb8bd4a1490535d6
Muu79 <muu794@gmail.com>
2023-04-04 23:28:47 +0200

    added provider to uniswap.

--- components/UniSwap.tsx
+++ components/UniSwap.tsx
@@ -1,12 +1,24 @@
 'use client'
 
 import { SwapWidget } from "@uniswap/widgets"
+import { useContext, useEffect, useState } from "react"
+import { ClientContext } from "./ClientProvider"
+import { ethers } from "ethers"
 
 export default () => {
+    const {provider} = useContext(ClientContext)
+    const [prov, setProv] = useState<ethers.providers.Web3Provider>();
 
+    useEffect(()=>{
+        if(!provider) return;
+        setProv( new ethers.providers.Web3Provider(
+            provider as ethers.providers.ExternalProvider
+        ));
+    },[provider])
+    
     return (<>
         <div className="Uniswap">
-            <SwapWidget />
+            <SwapWidget provider={prov} />
         </div>
     </>)
 }
